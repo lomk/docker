7960db3772cd4f464939311068e5af03d054c6c5
lomk <materynko@gmail.com>
2016-12-20 09:44:07 +0200

    32

--- runscript.sh
+++ runscript.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker run -it -v /opt/ansible:/etc/ansible ansible:v1
+docker run --rm -it \
+    -v /opt/ansible:/etc/ansible \
+    -v ~/.ssh/id_rsa:/root/.ssh/id_rsa \
+    -v ~/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub \
+    ansible:v1
